0417d389a7a872b045ffe9d0130593e1ec8424b1
n2k_ <71725716+neros2k@users.noreply.github.com>
2022-05-04 04:46:36 +0300

    Расширение функционала.

--- src/main/java/n2k/ntnofire/nTNoFire.java
+++ src/main/java/n2k/ntnofire/nTNoFire.java
@@ -1,14 +1,19 @@
 package n2k.ntnofire;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 public final class nTNoFire extends JavaPlugin implements Listener {
     @Override public void onEnable() {
         this.getServer().getPluginManager().registerEvents(new Listener() {
-            @EventHandler void onFire(@NotNull BlockBurnEvent EVENT) {
+            @EventHandler void onBlockBurn(@NotNull BlockBurnEvent EVENT) {
                 EVENT.setCancelled(true);
+            }
+            @EventHandler void onFirePlace(@NotNull BlockPlaceEvent EVENT) {
+                if(EVENT.getBlockPlaced().getType() == Material.FIRE) EVENT.setCancelled(true);
             }
         }, this);
     }
