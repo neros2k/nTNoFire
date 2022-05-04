35d102348f9f3d2898117058c4ebb7165fd39652
neros2k <neros2k@gmail.com>
2022-05-04 04:13:00 +0300

    Реализован основной функционал.

---
+++ src/main/java/n2k/ntnofire/nTNoFire.java
@@ -0,0 +1,16 @@
+package n2k.ntnofire;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+public final class nTNoFire extends JavaPlugin implements Listener {
+    @Override public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler
+            void onFire(@NotNull BlockBurnEvent EVENT) {
+                EVENT.setCancelled(true);
+            }
+        }, this);
+    }
+}
